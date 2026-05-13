bfe6b4cd75534845b0e0c6b6d545aad007351c23
sulochana <sulochanadananjani10@gmail.com>
2026-05-13 18:06:07 +0530

    update

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import { User } from "../entity/user";
 import { Order } from "../entity/order";
 import { Subscription } from "../entity/subscription";
+import path from "path";
 
 dotenv.config();
 
@@ -19,5 +20,5 @@ export const AppDataSource = new DataSource({
   logging: true,
 
   entities: [User, Order, Subscription],
-  migrations: ["src/migrations/*.ts"],
+  migrations: [path.join(__dirname, "../migrations/*.{ts,js}")],
 });
